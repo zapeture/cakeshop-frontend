655191df408aa248d115a97379b4d3bfb9b70de7
Fortune Zviregei <fortunechainz@gmail.com>
2021-07-10 23:05:40 +0200

    revalidate products v1.0.2

--- pages/products/index.js
+++ pages/products/index.js
@@ -156,6 +156,7 @@ export async function getStaticProps() {
   return {
     props: {
       products: products,
+      revalidate: 5,
     },
   };
 }
--- utils/urls.js
+++ utils/urls.js
@@ -1,5 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_URL || 'http://localhost:1337';
+  process.env.NEXT_PUBLIC_API_URL || 'https://cakeshop-backend.herokuapp.com';
 
 export const MAGIC_PUBLIC_KEY = process.env.NEXT_PUBLIC_MAGIC_PUBLIC_KEY;
 
